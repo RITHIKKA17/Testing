4182e1ba102ae6c48604d896567ecbc678b29e00
RITHIKKA17 <727821tuec156@skct.edu.in>
2023-06-07 11:55:49 +0530

    day-7

---
+++ TestNG/Day_7/src/test/java/com/selenium/Day_7/Testcase_1.java
@@ -0,0 +1,35 @@
+package com.selenium.Day_7;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+
+public class Testcase_1 {
+	public WebDriver dr;
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  WebDriverManager.chromedriver().setup();
+	  dr= new ChromeDriver();
+	  dr.manage().window().maximize();
+	  dr.get("https://demo.wpeverest.com/user-registration/guest-registration-form/");
+	  String title="Guest Registration Form";
+	  String ctitle=dr.getTitle();
+	  Assert.assertEquals(title, ctitle);
+	  
+  }
+
+  @AfterTest
+  public void afterTest() {
+	  dr.quit();
+  }
+
+}
